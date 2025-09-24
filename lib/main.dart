ffa34a1d40f784db2100d0a42bce4d22db5f38a5
Thanush-07 <178015554+Thanush-07@users.noreply.github.com>
2025-09-24 15:22:01 +0530

    main dart file updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:get/get.dart';
 import './src/screens/registration.dart';
-import 'src/screens/home.dart';
+import './src/screens/home.dart';
 import './utils/constants.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await Hive.openBox(kHiveBox);
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       locale: const Locale('ta'),
       supportedLocales: const [
         Locale('ta'),
@@ -29,7 +29,12 @@ class MyApp extends StatelessWidget {
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      // home: ...
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/register', // 👈 Start with home.dart
+      getPages: [
+        GetPage(name: '/home', page: () => HomeScreen()),
+        GetPage(name: '/register', page: () => RegistrationPage()),
+      ],
     );
   }
 }
