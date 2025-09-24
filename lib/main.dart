e7983e08f7df53e564c6d8f154af9791bbdbf4e3
Thanush-07 <178015554+Thanush-07@users.noreply.github.com>
2025-09-25 01:09:04 +0530

    new pages updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:get/get.dart';
-import './src/screens/registration.dart';
-import './src/screens/home.dart';
-import './utils/constants.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter_map_tile_caching/flutter_map_tile_caching.dart';
 
-void main() async {
+// Screens
+import 'src/screens/splash.dart';
+import 'src/screens/login.dart';
+import 'src/screens/registration.dart';
+import 'src/screens/home.dart';
+import 'src/screens/map.dart';
+import 'src/screens/weather.dart';
+import 'src/screens/profile.dart';
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Hive.initFlutter();
-  await Hive.openBox(kHiveBox);
+  await Hive.openBox('fisherman_box');
+
+  try {
+    await FMTCObjectBoxBackend().initialise();
+    await FMTCStore('mapStore').manage.create();
+  } catch (e) {
+    debugPrint('FMTC init error: $e');
+  }
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
+      title: 'Sea Siren Alert',
       locale: const Locale('ta'),
-      supportedLocales: const [
-        Locale('ta'),
-        Locale('en', 'US'),
-      ],
+      supportedLocales: const [Locale('ta'), Locale('en', 'US')],
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/register', // 👈 Start with home.dart
-      getPages: [
-        GetPage(name: '/home', page: () => HomeScreen()),
-        GetPage(name: '/register', page: () => RegistrationPage()),
-      ],
+      home: const SplashScreen(),
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/register': (context) => const RegistrationPage(),
+        '/home': (context) => const HomeScreen(),
+        '/map': (context) => const OfflineMapScreen(),
+        '/weather': (context) => const WeatherAlertScreen(),
+        '/profile': (context) => const ProfileScreen(),
+      },
     );
   }
 }
